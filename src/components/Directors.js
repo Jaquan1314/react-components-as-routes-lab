a1cc6c676a0cb4ee3eea4a5b357ec13ea478b7d5
Jaquan1314 <javaughngrant@gmail.com>
2020-12-20 13:53:54 -0500

    commit tree 08b298c617d380e70ebaab5faae925f3e611297e

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -1,12 +1,25 @@
-import React from 'react';
-import { directors } from '../data';
+import React from "react";
+import { directors } from "../data";
 
 const Directors = () => {
+  console.log(directors);
+  let eachDirector = directors.map((director) => (
+    <div>
+      {director.name}
+      <ul>
+        {director.movies.map((movie) => (
+          <li>{movie}</li>
+        ))}
+      </ul>
+    </div>
+  ));
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Directors Page</h1>
+      {eachDirector}
     </div>
   );
-}
+};
 
-export default Directors
+export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -1,10 +1,24 @@
-import React from 'react';
-import { movies } from '../data';
+import React from "react";
+import { movies } from "../data";
 
 const Movies = () => {
+  console.log(movies);
+  let eachMovie = movies.map((movie) => (
+    <div>
+      {movie.title} - {movie.time} minutes
+      <ul>
+        {movie.genres.map((genre) => (
+          <li>{genre}</li>
+        ))}
+      </ul>
+    </div>
+    // ul with a list of its genres
+  ));
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {eachMovie}
     </div>
   );
 };
